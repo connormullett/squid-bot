ed82184151f871d53d6a7f17d97596e78b8a2bc0
connor mullett <connormullett@gmail.com>
2026-07-10 00:45:20 +0000

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/connormullett/squid-bot
+
+go 1.26.1
+
+require (
+	github.com/go-telegram/bot v1.22.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/hashicorp/golang-lru/v2 v2.0.7
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"math/rand"
+	"os"
+	"path/filepath"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	lru "github.com/hashicorp/golang-lru/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println(err)
+	}
+
+	b, err := bot.New(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Fatalf("Failed to create bot: %s", err.Error())
+	}
+	log.Println("Created bot")
+
+	cache, _ := lru.New[string, string](12)
+
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/squid", bot.MatchTypePrefix, makeHandleGetSquid(cache))
+}
+
+func makeHandleGetSquid(cache *lru.Cache[string, string]) bot.HandlerFunc {
+	return func(c context.Context, b *bot.Bot, update *models.Update) {
+		file, err := getRandomFile(cache, os.Getenv("SQUID_DIR"))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if file == "" {
+			log.Println("No files found")
+			return
+		}
+
+		if err := sendPhoto(c, b, update, file); err != nil {
+			log.Println(err)
+			return
+		}
+		log.Printf("Sent photo: %+v", file)
+	}
+}
+
+func sendPhoto(c context.Context, b *bot.Bot, update *models.Update, file string) error {
+	photoContent, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	_, err = b.SendPhoto(c, &bot.SendPhotoParams{
+		ChatID: update.Message.Chat.ID,
+		Photo: &models.InputFileUpload{
+			Filename: filepath.Base(file),
+			Data:     bytes.NewReader(photoContent),
+		},
+	})
+
+	return err
+}
+
+func getRandomFile(cache *lru.Cache[string, string], dirPath string) (string, error) {
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		return "", err
+	}
+
+	var files []string
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			files = append(files, entry.Name())
+		}
+	}
+
+	for {
+		if len(files) == 0 {
+			return "", nil
+		}
+
+		randomIndex := rand.Intn(len(files))
+		randomFileName := files[randomIndex]
+
+		path := filepath.Join(dirPath, randomFileName)
+
+		if cache.Contains(path) {
+			continue
+		}
+		cache.Add(path, path)
+		return path, nil
+	}
+}
